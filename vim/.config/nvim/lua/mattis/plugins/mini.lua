e5b265c9de0850806c92c4345839c74b6463f28c
Mattis Megevand <megevand.mattis@gmail.com>
2024-06-28 22:59:03 +0200

    vim: status line update

--- vim/.config/nvim/lua/mattis/plugins/mini.lua
+++ vim/.config/nvim/lua/mattis/plugins/mini.lua
@@ -7,7 +7,55 @@ return {
       require('mini.pairs').setup()
 
       local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      statusline.setup {
+        content = {
+          active = function()
+            local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+            local git = MiniStatusline.section_git { trunc_width = 40 }
+            local filename = MiniStatusline.section_filename { trunc_width = 140 }
+            local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+            local location = MiniStatusline.section_location { trunc_width = 75 }
+            local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+            return MiniStatusline.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
+            }
+          end,
+        },
+        use_icons = vim.g.have_nerd_font,
+      }
+
+      local orig_sm = statusline.section_mode
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_mode = function(args)
+        local mode, mode_hl = orig_sm(args)
+        return string.upper(mode), mode_hl
+      end
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_fileinfo = function(args)
+        local filetype = vim.bo.filetype
+        if (filetype == '') or vim.bo.buftype ~= '' then
+          return ''
+        end
+        local file_name, file_ext = vim.fn.expand '%:t', vim.fn.expand '%:e'
+
+        if vim.g.have_nerd_font then
+          local devicons = require 'nvim-web-devicons'
+          local icon = devicons.get_icon(file_name, file_ext, { default = true })
+          if icon ~= '' then
+            filetype = string.format('%s %s', icon, filetype)
+          end
+        end
+
+        return filetype
+      end
 
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
