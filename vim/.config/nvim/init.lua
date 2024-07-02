1d2ff4f25ceb638e7e5dfd3bdb49dc92f9066a2c
Mattis Megevand <megevand.mattis@gmail.com>
2024-07-02 22:55:35 +0200

    vim: allow differentiation work/personal config

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
+vim.g.work = vim.fn.hostname() ~= 'macbook.local'
+vim.g.have_nerd_font = not vim.g.work
 
 require 'options'
 require 'keymaps'
